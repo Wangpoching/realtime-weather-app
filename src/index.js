13fff07d886ed6bbb95f60ff6d6a723ccd069a10
Peter Wang <wangpeter588@gmail.com>
2021-11-25 21:44:55 +0800

    finish

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import WeatherApp from "./WeatherApp";
+import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById("root");
 function App() {
   return <WeatherApp />;
 }
 ReactDOM.render(<App />, rootElement);
+
+serviceWorker.register()
